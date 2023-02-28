73459efaffa59f10c7f96e793cc3c94603e90947
burakkaanozbek <burak.ozbek@binalyze.com>
2023-03-01 00:02:43 +0300

    Prepare functions

--- main.go
+++ main.go
@@ -1,30 +1,106 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/rakyll/openai-go"
+	"github.com/rakyll/openai-go/completion"
+)
+
+var (
+	promptPrefix = "I will give you an articles about DFIR in various categories Please extract maximum three keywords from the articles to create insights. Use the following format: Keywords: keyword1, keyword2, keyword3. Use only lowercase letters \n"
+	newsDir      = "news"
+	textList     []string
 )
 
 func main() {
-	var news []string
-	dir := "news"
-	files, err := ioutil.ReadDir(dir)
+	newsFiles, err := os.ReadDir(newsDir)
 	if err != nil {
-		fmt.Println("Error reading directory:", err)
+		log.Printf("Failed to read news directory '%s' error: %v", newsDir, err)
 		os.Exit(1)
 	}
-	for _, file := range files {
-		if file.Mode().IsRegular() {
-			path := filepath.Join(dir, file.Name())
-			data, err := ioutil.ReadFile(path)
+	for _, file := range newsFiles {
+		if !file.IsDir() && file.Type().IsRegular() {
+			path := filepath.Join(newsDir, file.Name())
+			text, err := os.ReadFile(path)
 			if err != nil {
-				fmt.Println("Error reading file:", err)
+				log.Printf("Failed to read file '%s' error: %v", path, err)
 				continue
 			}
-			news = append(news, string(data))
+			textList = append(textList, string(text))
 		}
 	}
-	fmt.Println(news[0])
+
+	extractKeywords(textList)
+}
+
+func extractKeywords(texts []string) {
+	ctx := context.Background()
+	s := openai.NewSession(os.Getenv("OPENAI_API_KEY"))
+	client := completion.NewClient(s, "text-davinci-003")
+	var keywordList []string
+	for _, text := range texts {
+		resp, err := client.Create(ctx, &completion.CreateParameters{
+			N:         1,
+			MaxTokens: 200,
+			Prompt:    []string{promptPrefix + text}})
+		if err != nil {
+			log.Fatalf("Failed to create completion error: %v", err)
+		}
+
+		for _, choice := range resp.Choices {
+			keywordList = append(keywordList, choice.Text)
+		}
+	}
+
+	saveKeywords(keywordList)
+}
+
+func saveKeywords(keywords []string) {
+	f, err := os.Create("keywords.txt")
+	if err != nil {
+		log.Fatalf("Failed to create keywords file error: %v", err)
+	}
+	defer f.Close()
+
+	var filteredKeywords []string
+	for _, keyword := range keywords {
+		if strings.TrimSpace(keyword) != "" {
+			filteredKeywords = append(filteredKeywords, keyword)
+		}
+	}
+
+	// Write filtered array to file
+	data := []byte(strings.Join(filteredKeywords, "\n"))
+	err = os.WriteFile(f.Name(), data, 0644)
+	if err != nil {
+		log.Printf("Failed to write keywords to file error: %v", err)
+		return
+	}
+
+	readKeywordsFile(f)
+}
+
+func readKeywordsFile(f *os.File) {
+	textData, err := os.ReadFile(f.Name())
+	if err != nil {
+		log.Printf("Failed to read keywords file error: %v", err)
+	}
+
+	// Remove empty lines and "Keywords:" prefix
+	lines := strings.Split(string(textData), "\n")
+	var filteredLines []string
+	for _, line := range lines {
+		if strings.TrimSpace(line) != "" {
+			line = strings.TrimPrefix(line, "Keywords:")
+			filteredLines = append(filteredLines, strings.TrimSpace(line))
+		}
+	}
+
+	fmt.Println(filteredLines)
 }
